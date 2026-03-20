31912a6cc60ba55ca528a5671ca2b39723600a6e
gaurav porwal <gauravprwl14@gmail.com>
2026-03-20 05:16:06 +0000

    feat(14-algorithms): Add section structure and index

---
+++ docs-site/pages/14-algorithms/_meta.js
@@ -0,0 +1,7 @@
+export default {
+  index: "Overview",
+  concepts: "📖 Concepts",
+  distributed: "🌐 Distributed Algorithms",
+  "interview-patterns": "🎯 Interview Patterns",
+  "hands-on": "🔬 Hands-On"
+}
