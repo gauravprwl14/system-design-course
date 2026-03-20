1f7cab59630606a996b1181c27e33c6dba122d4a
gaurav porwal <gauravprwl14@gmail.com>
2026-03-20 04:50:07 +0000

    feat: Add navigation entries for CDC and data warehouse articles

---
+++ docs-site/pages/01-databases/concepts/_meta.js
@@ -0,0 +1,5 @@
+export default {
+  "column-store-databases": "Column-Store Databases",
+  "change-data-capture": "Change Data Capture (CDC)",
+  "data-warehouse-vs-lake": "Data Warehouse vs Lake vs Lakehouse"
+}
