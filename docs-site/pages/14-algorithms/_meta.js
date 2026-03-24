fdde3c19f45efaaa23307b42f151272f2b0ded82
gaurav porwal <gauravprwl14@gmail.com>
2026-03-24 13:16:07 +0000

    chore: Remove wrongly-placed pages/14-algorithms files (content is in content/14-algorithms/)

--- docs-site/pages/14-algorithms/_meta.js
+++
@@ -1,7 +0,0 @@
-export default {
-  index: "Overview",
-  concepts: "📖 Concepts",
-  distributed: "🌐 Distributed Algorithms",
-  "interview-patterns": "🎯 Interview Patterns",
-  "hands-on": "🔬 Hands-On"
-}
